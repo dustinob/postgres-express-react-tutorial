c567fd9b9ee8c08ab7ba6e4b0ec25c5960ab9cd0
Dustin O'Brien <dcobrien82@gmail.com>
2017-11-06 12:53:57 -0800

    server setup

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const logger = require('morgan');
+const bodyParser = reqire('body-parser');
+
+// Set up the express app
+const app = express();
+
+// Log requests to console.
+app.use(logger('dev'));
+
+// Parse incoming requests data (https://github.com/expressjs/bodsy-parser)
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false}));
+
+// Setup a default catch-all route that send back a welcome message in JSON format.
+app.get('*', (req, res) => res.status(200).send({
+  message: 'Welcome to the beginning of nothingness.'
+}));
+
+module.exports = app;
