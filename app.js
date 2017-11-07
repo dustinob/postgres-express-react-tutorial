1d4f86457b7a31dea8bedf1d5511e467749a3817
Dustin O'Brien <dcobrien82@gmail.com>
2017-11-07 14:33:06 -0800

    postgres database setup

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const logger = require('morgan');
-const bodyParser = reqire('body-parser');
+const bodyParser = require('body-parser');
 
 // Set up the express app
 const app = express();
@@ -14,7 +14,7 @@ app.use(bodyParser.urlencoded({ extended: false}));
 
 // Setup a default catch-all route that send back a welcome message in JSON format.
 app.get('*', (req, res) => res.status(200).send({
-  message: 'Welcome to the beginning of nothingness.'
+  message: 'Welcome to the beginning of nothingness.',
 }));
 
 module.exports = app;
